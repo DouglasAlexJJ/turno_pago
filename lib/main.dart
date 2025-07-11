ad2c7173682e79a8081ba967e9e42c1be3a132ae
DouglasAlexJJ <douglasalexjj@outlook.com.br>
2025-07-11 15:12:40 -0300

    firebase install
    instação com sucesso do firebase para implementação do salvamento em nuvem.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 // lib/main.dart
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Importe o Firebase Core
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:turno_pago/firebase_options.dart'; // Importe o arquivo gerado
 import 'package:turno_pago/screens/main_screen.dart';
 import 'package:turno_pago/screens/primeiro_acesso_screen.dart';
 import 'package:turno_pago/services/background_service.dart';
 
 Future<void> main() async {
-  // Garante que os bindings do Flutter foram inicializados
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inicializa o serviço de background
+  // INICIALIZA O FIREBASE
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await initializeService();
 
-  // Verifica se é o primeiro acesso usando a flag
   final prefs = await SharedPreferences.getInstance();
   final bool primeiroAcessoConcluido = prefs.getBool('primeiro_acesso_concluido') ?? false;
 
@@ -34,7 +38,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      // Decide qual será a tela inicial com base na flag
       home: primeiroAcessoConcluido ? const MainScreen() : const PrimeiroAcessoScreen(),
     );
   }
